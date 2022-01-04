6740757796ed136519d2ca8ec98a9e76b40c527a
Drew Speed <drewjspeed@gmail.com>
2022-01-04 10:58:48 -0600

    fixed empty player name issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,8 +169,17 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name=prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -197,7 +206,8 @@ var playerInfo = {
     window.alert("You don't have enough money!");
   }
 
-};
+}
+}
 
 var enemyInfo = [
   {
